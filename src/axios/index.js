821db897a000df3b57afce43bd470a40cb98a1cf
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2021-11-29 19:14:35 +0300

    .

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: 'http://node2-env-1.eba-3jaztibh.us-east-2.elasticbeanstalk.com/',
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://node2-env-1.eba-3jaztibh.us-east-2.elasticbeanstalk.com/',
+    // baseURL: 'http://localhost:8080/',
 })
